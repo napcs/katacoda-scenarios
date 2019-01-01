5e96deebd21ac8bc8b2ab0f80849888b5e581553
Brian Hogan <brianhogan@napcs.com>
2019-01-01 16:54:37 -0600

    make code run foreground so it works

--- smallsharptools/start.sh
+++ smallsharptools/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Create directory structure
+mkdir /home/scrapbook/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+
+# mod the prompt so it looks like a typical prompt.
+echo "export PS1=\"\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\h:\W\\$ \[$(tput sgr0)\]"\" >> /home/scrapbook/.bashrc
+
+# let's get out of the `tutorial` directory - can't figure out what puts me in there
+# by default
 cd
+
+# source the bashrc so the settings take effect.
 source .bashrc
+
+# clear the screen so the reader doesn't see any of this junk
 clear

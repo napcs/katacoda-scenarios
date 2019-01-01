629f21c5b0c23ff66fc6be7eb3e60558cc75c1ed
Brian Hogan <brianhogan@napcs.com>
2019-01-01 16:46:59 -0600

    more content that doesn't seem to work right

--- smallsharptools/configure_env.sh
+++ smallsharptools/configure_env.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
 # This doesn't seem to work at all. Background scripts are broken as of 1/1/2019
 # so I've moved everything to foreground which does work.
 
-cd
 mkdir /home/scrapbook/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
 echo "export PS1=\"\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\h:\W\\$ \[$(tput sgr0)\]"\" >> /home/scrapbook/.bashrc

6891705e45414de7b9a73bcaba99bbe66a017b9e
Brian Hogan <brianhogan@napcs.com>
2019-01-01 16:56:56 -0600

    remove check for directory pwd as it is not supported

--- smallsharptools/changing_dirs/verify.sh
+++ smallsharptools/changing_dirs/verify.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-[ $(pwd) == "/home/scrapbook/Desktop" ] && echo "done"
+# unable to check current dir with pwd unfortunately.
+# [ $(pwd) == "/home/scrapbook/Desktop" ] && echo "done"
+
+echo "done"

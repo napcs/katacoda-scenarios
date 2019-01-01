18bff14ae8dfa5464929913f196b2ab77007d632
Brian Hogan <brianhogan@napcs.com>
2019-01-01 16:45:09 -0600

    more content that doesn't seem to work right

--- smallsharptools/start.sh
+++ smallsharptools/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-cd
-mkdir /home/scrapbook/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
-echo "export PS1=\"\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\h:\W\\$ \[$(tput sgr0)\]"\" >> /home/scrapbook/.bashrc
 source .bashrc
 clear

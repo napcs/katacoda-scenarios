8b19d4d75b996b50bf3a811f511542344f9a8329
Brian Hogan <brianhogan@napcs.com>
2019-01-01 14:49:25 -0600

    trying to get the env set up

--- smallsharptools/start.sh
+++ smallsharptools/start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd
 mkdir Desktop Documents Downloads Music Pictures Public Templates Videos;
 echo "export PS1=\"\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\h:\W\\$ \[$(tput sgr0)\]"\" >> ~/.bashrc

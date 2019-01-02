d8d294a816affe22d2b0487ae4279048c001c46f
Brian Hogan <brianhogan@napcs.com>
2019-01-01 18:13:53 -0600

    dictionary words

--- smallsharptools/start.sh
+++ smallsharptools/start.sh
@@ -10,6 +10,11 @@ echo "export PS1=\"\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\h:\W\\$
 # by default
 cd
 
+# need the dictionary words because they're not installed.
+# TODO: replace with a different example or upload the file rather than
+# rely on 3rd party
+curl https://raw.githubusercontent.com/eneko/data-repository/master/data/words.txt -o /usr/share/dict/words
+
 # source the bashrc so the settings take effect.
 source .bashrc
 

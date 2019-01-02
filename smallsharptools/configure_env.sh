1e63253a18c1da26cd269b17d1dacdcb8ce8228f
Brian Hogan <brianhogan@napcs.com>
2019-01-01 18:15:19 -0600

    testing a theory about config

--- smallsharptools/configure_env.sh
+++ smallsharptools/configure_env.sh
@@ -1,3 +1,8 @@
-# This doesn't seem to work at all. Background scripts are broken as of 1/1/2019
-# so I've moved everything to foreground which does work.
-
+# Create directory structure
+mkdir /home/scrapbook/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+# mod the prompt so it looks like a typical prompt.
+echo "export PS1=\"\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\h:\W\\$ \[$(tput sgr0)\]"\" >> /home/scrapbook/.bashrc
+# need the dictionary words because they're not installed.
+# TODO: replace with a different example or upload the file rather than
+# rely on 3rd party
+curl https://raw.githubusercontent.com/eneko/data-repository/master/data/words.txt -o /usr/share/dict/words
--- smallsharptools/start.sh
+++ smallsharptools/start.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# Create directory structure
-mkdir /home/scrapbook/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
-
-# mod the prompt so it looks like a typical prompt.
-echo "export PS1=\"\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\h:\W\\$ \[$(tput sgr0)\]"\" >> /home/scrapbook/.bashrc
 
 # let's get out of the `tutorial` directory - can't figure out what puts me in there
 # by default
 cd
-
-# need the dictionary words because they're not installed.
-# TODO: replace with a different example or upload the file rather than
-# rely on 3rd party
-curl https://raw.githubusercontent.com/eneko/data-repository/master/data/words.txt -o /usr/share/dict/words
 
 # source the bashrc so the settings take effect.
 source .bashrc

a8a14670b95b5a14b9e06d98216ffdd508e0a8d0
Brian Hogan <brianhogan@napcs.com>
2019-01-01 18:17:47 -0600

    testing a theory about config

--- smallsharptools/configure_env.sh
+++ smallsharptools/configure_env.sh
@@ -1,7 +1,9 @@
 # Create directory structure
 mkdir /home/scrapbook/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+
 # mod the prompt so it looks like a typical prompt.
 echo "export PS1=\"\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\h:\W\\$ \[$(tput sgr0)\]"\" >> /home/scrapbook/.bashrc
+
 # need the dictionary words because they're not installed.
 # TODO: replace with a different example or upload the file rather than
 # rely on 3rd party

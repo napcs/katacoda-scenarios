bfcbf68a89a4e21521ef0adc08742fce230d0c53
Brian Hogan <brianh@tailscale.com>
2025-07-22 09:16:24 -0500

    attempt to fix directory change test

--- smallsharptools/changing_dirs/verify.sh
+++ smallsharptools/changing_dirs/verify.sh
@@ -1,1 +1,12 @@
-export PWD=$(pwdx 1) && [ "$PWD" == "1: ~/Desktop" ] && echo "done"
+#!/usr/bin/env bash
+pid_file="$HOME/.user_shell.pid"
+
+cwd= $(readlink -f "/proc/${pid}/cwd")
+
+if [[ ${cwd} == "${HOME}/Desktop" ]]; then
+  echo "done"
+  exit 0
+else
+  echo "not yet (cwd = ${cwd})"
+  exit 1
+fi
--- smallsharptools/start.sh
+++ smallsharptools/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# save the ID of the session. We need this for pidx to find the shell.
+echo $$ > "$HOME/.user_shell.pid"     # $$ is *this* Bash’s PID :contentReference[oaicite:0]{index=0}
+
 # let's get out of the `tutorial` directory - can't figure out what puts me in there
 # by default
 cd
